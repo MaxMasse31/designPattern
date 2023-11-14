deb5a93bbf7c17b65660ad1b22000037949f1b88
Max Masse <maxmasse199@gmail.com>
2023-11-14 13:10:52 -0500

    config constructor Pattern

--- js/App.js
+++ js/App.js
@@ -1,17 +1,17 @@
 class App {
-    constructor() {
-        this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        this.moviesApi = new MovieApi('/data/movie-data.json')
-    }
+  constructor() {
+      this.$moviesWrapper = document.querySelector('.movies-wrapper')
+      this.moviesApi = new MovieApi('/data/old-movie-data.json')
+  }
 
-    async main() {
-        const movies = await this.moviesApi.getMovies()
+  async main() {
+      const movies = await this.moviesApi.getMovies()
 
-        movies.forEach(movie => {
-            const Template = new MovieCard(movie)
-            this.$moviesWrapper.appendChild(Template.createMovieCard())        
-        })    
-    }
+      movies.forEach(movie => {
+          const Template = new MovieCard(movie)
+          this.$moviesWrapper.appendChild(Template.createMovieCard())        
+      })    
+  }
 }
 
 const app = new App()
