7d7138bcf56efe6973e7efcaa7cd6c13bc6605d0
Max Masse <maxmasse199@gmail.com>
2023-11-15 15:11:02 -0500

    ajout modal

--- js/App.js
+++ js/App.js
@@ -1,35 +1,47 @@
 // App.js
+
+// Importation des modules et composants nécessaires
 import { MovieApi } from "./api/Api.js";
 import { MovieCard } from "./templates/MovieCard.js";
 import { MoviesFactory } from "./factories/MoviesFactory.js";
 
+// Classe principale de l'application
 class App {
   constructor() {
-      this.$moviesWrapper = document.querySelector('.movies-wrapper')
-      
-      this.moviesApi = new MovieApi('/data/new-movie-data.json')
-      this.externalMoviesApi = new MovieApi('/data/external-movie-data.json')
+    // Éléments DOM pour les films et la fenêtre modale
+    this.$moviesWrapper = document.querySelector(".movies-wrapper");
+    this.$modalWrapper = document.querySelector(".modal");
+
+    // Initialisation des API de films
+    this.moviesApi = new MovieApi("/data/new-movie-data.json");
+    this.externalMoviesApi = new MovieApi("/data/external-movie-data.json");
   }
 
+  // Fonction principale pour initialiser l'application
   async main() {
-      const moviesData = await this.moviesApi.get()
-      const externalMoviesData = await this.externalMoviesApi.get()
+    // Récupération des données des films à partir des deux API
+    const moviesData = await this.moviesApi.get();
+    const externalMoviesData = await this.externalMoviesApi.get();
 
-      const Movies = moviesData.map(movie => new MoviesFactory(movie, 'newApi'))
-      const ExternalMovies = externalMoviesData.map(movie => new MoviesFactory(movie, 'externalApi'))
+    // Ajout d'une fenêtre modale au site
+    const ModalForm = new Form();
+    ModalForm.render();
 
-      console.log(Movies)
+    // Création d'objets Movie à l'aide de MoviesFactory pour les deux API
+    const Movies = moviesData.map(
+      (movie) => new MoviesFactory(movie, "newApi")
+    );
+    const ExternalMovies = externalMoviesData.map(
+      (movie) => new MoviesFactory(movie, "externalApi")
+    );
 
-      const FullMovies = Movies.concat(ExternalMovies)
+    // Combinaison des films provenant des deux API
+    const FullMovies = Movies.concat(ExternalMovies);
 
-      FullMovies.forEach(movie => {
-              const Template = new MovieCard(movie)
-              this.$moviesWrapper.appendChild(
-                  Template.createMovieCard()
-              )
-          })
+    // Affichage des cartes de films sur le site
+    FullMovies.forEach((movie) => {
+      const Template = new MovieCard(movie);
+      this.$moviesWrapper.appendChild(Template.createMovieCard());
+    });
   }
 }
-
-const app = new App()
-app.main()
