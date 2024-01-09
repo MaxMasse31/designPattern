4f60d82ebf67767456d44aba8a7472a781b98343
Max Masse <maxmasse199@gmail.com>
2024-01-09 15:20:08 -0500

    base Template Pattern

---
+++ js/Search/MainSearch.js
@@ -0,0 +1,35 @@
+export class Search {
+    constructor(Movies) {
+        this.Movies = Movies
+    }
+
+    search(query) {
+        return this.filterMovies(query)
+    }
+}
+
+
+export class MovieNameSearch extends Search {
+    constructor(Movies) {
+        super(Movies)
+    }
+
+    filterMovies(query) {
+        return this.Movies.filter(Movie =>
+            Movie.title.toLowerCase().includes(query.toLowerCase())
+        )
+    }
+}
+
+
+export class ActorNameSearch extends Search {
+    constructor(Movies) {
+        super(Movies)
+    }
+
+    filterMovies(query) {
+        return this.Movies.filter(Movie =>
+            Movie.actor.toLowerCase().includes(query.toLowerCase())
+        )
+    }
+}
